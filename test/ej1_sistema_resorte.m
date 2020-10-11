62fc347a3ac131b00a4c1394e18123bee6f64668
Sebastian D <jsduenass@unal.edu.co>
2020-10-11 00:24:31 -0500

    corrección codigo duplicado ej1 y cambio nomenclatura conectividades  ej1

--- test/ej1_sistema_resorte.m
+++ test/ej1_sistema_resorte.m
@@ -1,9 +1,11 @@
 clc, clear, close 'all'
 
 %% ejercicio 1 sistema de resortes
+% solución de sistema unidmensional de resortes
+
 n=3;                % numero de nodos 
 % conectividad entre nodos [nodosA, nodosB, rigidez]
-conectividades = csvread( "conectividad.csv")            
+conectividades = csvread( "ej1_conectividad.csv")            
             
 nodosR=[1];           % nodos Restrigidos
 
@@ -22,24 +24,4 @@ disp(Fr)
 
 disp("Matriz de rigidez")
 disp(M)
-
-clear, clc, close 'all'
-% function handle
-
-
-%% ejercicio 1 sistema de resortes
-% solución de sistema unidmensional de resortes
-
-n=3;                % numero de nodos 
-% conectividad entre nodos [nodosA, nodosB, rigidez]
-conectividades = csvread( "conectividad3.csv")            
-            
-nodosR=[1];           % nodos Restrigidos
-
-Fe=zeros(n,1);          % Fuerzas externas
-Fe(2)=45;
-
-Fe(3)=30;
-
-[u,Fr,M]=sistema_resorte(n,conectividades,nodosR,Fe)
 
