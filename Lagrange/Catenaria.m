223a49ea95aed928b04ecb4f46630ad768a3c301
cychitivav <cychitivav@unal.edu.co>
2020-10-26 20:08:23 -0500

    elementos cateneria solucionados

--- Lagrange/Catenaria.m
+++ Lagrange/Catenaria.m
@@ -2,14 +2,15 @@ clear
 clc
 close all
 
-L = 8; % Longitud de la cuerda
-n = 60; % Numero de elementos
+L = 3.001; % Longitud de la cuerda
+n = 100; % Numero de elementos
 th = 0.1; % Grosor de la cuerda
 m = 10; % Masa total de la cuerda
+le = L/n;
 
 g = [0, -9.81]; % Aceleracion de la gravedad
 P0 = [0,0];
-Pf = [4,-2];
+Pf = [3,0];
 
 lambda = Pf(2)-P0(2);
 d = Pf(1)-P0(1);
@@ -17,38 +18,53 @@ d = Pf(1)-P0(1);
 syms alpha;
 a = double(vpasolve(2*sinh(alpha*d/2)==alpha*sqrt(L^2-lambda^2), alpha,10));
 
-u = fsolve(@(u)catenary(u,a,P0,Pf),P0);
+C = fsolve(@(C)catenary(C,a,P0,Pf),P0);
 
-x = linspace(P0(1),Pf(1),n);
-y=1/a*cosh(a*(x+u(1)))+u(2);
+x = [];
+y = [];
 
-angulo=atan( sinh(a*(x+u(1)))  );
-xb=0;       yb=0;
-le=L/(n);
+x(1)=P0(1);
+x(2)=P0(1)+le/1000;
+y(1) = 1/a*cosh(a*(x(1)+C(1)))+C(2);
+y(2) = 1/a*cosh(a*(x(2)+C(1)))+C(2);
 
-le=sqrt(diff(x).^2+diff(y).^2);
+angulo = atan2(y(2)-y(1),x(2)-x(1));
 
-for k=1:n-1 
-    
-    xc(k)=le(k)/2*cos(angulo(k))+ xb(k) ;
-    yc(k)=le(k)/2*sin(angulo(k))+ yb(k);
-    
-    xb(k+1)=xb(k)+le(k)*cos(angulo(k));
-    yb(k+1)=yb(k)+le(k)*sin(angulo(k));
+for i=2:n+1
+    x(i) = x(i-1)+le*cos(angulo(i-1));
+    y(i) = 1/a*cosh(a*(x(i)+C(1)))+C(2);
+    angulo(i) = atan2(y(i)-y(i-1),x(i)-x(i-1));
 end
 
-plot(x,y,":")
-hold on 
-plot(xc,yc,".")
-plot(xb,yb)
+scatter(x,y)
+hold on
+plot(x,y,"--")
+
+% angulo = atan2(diff(y),diff(x));
+% xb=0;       yb=0;
+% 
+% le=sqrt(diff(y).^2+diff(x).^2);
+% 
+% 
+% for k=1:n-1
+%     xc(k)=le(k)/2*cos(angulo(k))+ xb(k) ;
+%     yc(k)=le(k)/2*sin(angulo(k))+ yb(k);
+%     
+%     xb(k+1)=xb(k)+le(k)*cos(angulo(k));
+%     yb(k+1)=yb(k)+le(k)*sin(angulo(k));
+% end
+% 
+% plot(x,y,"--")
+% hold on 
+% plot(xc,yc,"o")
+% plot(xb,yb)
+% %plot(x,sinh(a*(x+C(1))))
 
 axis equal
-%set(gca,'DataAspectRatio',[1 1 1])
 
-
-
-function F = catenary(u,a,P0,Pf)
-    F(1) = 1/a*cosh(a*(P0(1)+u(1)))+u(2)-P0(2);
-    F(2) = 1/a*cosh(a*(Pf(1)+u(1)))+u(2)-Pf(2);
+function F = catenary(C,a,P0,Pf)
+    F(1) = 1/a*cosh(a*(P0(1)+C(1)))+C(2)-P0(2);
+    F(2) = 1/a*cosh(a*(Pf(1)+C(1)))+C(2)-Pf(2);
 end
+
 
